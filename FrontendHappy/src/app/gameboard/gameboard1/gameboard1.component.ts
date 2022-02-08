f559b8bb26fbfaff06b8563f0b2987f1664d4ad0
Morales-10 <anna450s@easv365.dk>
2022-02-08 10:41:23 +0100

    frontend modules

---
+++ FrontendHappy/src/app/gameboard/gameboard1/gameboard1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gameboard1',
+  templateUrl: './gameboard1.component.html',
+  styleUrls: ['./gameboard1.component.css']
+})
+export class Gameboard1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ FrontendHappy/src/app/gameboard/gameboard2/gameboard2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gameboard2',
+  templateUrl: './gameboard2.component.html',
+  styleUrls: ['./gameboard2.component.css']
+})
+export class Gameboard2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
