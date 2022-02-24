7fb5058c6c9be2e455e7a2751add2dc73cc181ce
HaraldurJoh <zeasick@gmail.com>
2022-02-24 13:07:42 +0100

    Login

--- FrontendHappy/src/app/app-routing.module.ts
+++ FrontendHappy/src/app/app-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import {Gameboard1Component} from "./gameboard/gameboard1/gameboard1.component";
 import {Gameboard2Component} from "./gameboard/gameboard2/gameboard2.component";
 import {CommonModule} from "@angular/common";
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
 
-
+const routes: Routes = [
+  {path:'', component:LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    Gameboard1Component,
-    Gameboard2Component
+    //Gameboard1Component,
+    //Gameboard2Component
   ],
-  imports: [CommonModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 
 })
 export class AppRoutingModule { }
--- FrontendHappy/src/app/app.module.ts
+++ FrontendHappy/src/app/app.module.ts
@@ -1,21 +1,38 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {Gameboard1Component} from "./gameboard/gameboard1/gameboard1.component";
 import {Gameboard2Component} from "./gameboard/gameboard2/gameboard2.component";
+import {RouterModule} from "@angular/router";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     Gameboard1Component,
-    Gameboard2Component
+    Gameboard2Component,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      RouterModule,
+      BrowserAnimationsModule,
+      FlexLayoutModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatButtonModule,
+      MatCardModule,
+      MatToolbarModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
