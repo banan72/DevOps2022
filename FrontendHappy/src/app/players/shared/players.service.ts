517ad9f33450b97be282feafa8bfbf1492c145df
pete9991 <peter.lydeking@gmail.com>
2022-03-31 12:40:11 +0200

    re-added ability to run/test locally

--- FrontendHappy/src/app/players/shared/players.service.ts
+++ FrontendHappy/src/app/players/shared/players.service.ts
@@ -12,9 +12,9 @@ export class PlayersService {
   }
 
   getAll(): Observable<PlayerDto[]>{
-    return this._http.get<PlayerDto[]>('http://185.51.76.42:8091/api/Player');
+    return this._http.get<PlayerDto[]>('https://localhost:5001/api/Player');
   }
   getTopX(id: number): Observable<PlayerDto[]>{
-    return this._http.get<PlayerDto[]>(`http://185.51.76.42:8091/api/Player/GetTopSippers?topPlayers=${id}`);
+    return this._http.get<PlayerDto[]>(`https://localhost:5001/api/Player/GetTopSippers?topPlayers=${id}`);
   }
 }
